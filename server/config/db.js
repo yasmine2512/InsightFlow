c882516b8302056d1a60ccfca8e49f855513bc0c
Yasmine <yasmineseddari32@gmail.com>
2026-08-01 21:54:27 +0100

    Add Playwright GitHub Actions

--- server/config/db.js
+++ server/config/db.js
@@ -1,6 +1,10 @@
 import mongoose from "mongoose";
-
+import dns from "dns";
 const connectDB = async () => {
+  dns.setServers(process.env.DNS_SERVERS
+    ? process.env.DNS_SERVERS.split(",")
+    : ["8.8.8.8", "1.1.1.1"]
+);
   try {
     const conn = await mongoose.connect(process.env.MONGO_URI);
     console.log(`MongoDB Connected: ${conn.connection.host}`);
