85d36399016d377b2a38781dff8f369a5ff163b6
Yasmine <yasmineseddari32@gmail.com>
2026-08-02 17:47:45 +0100

    updated test

--- client/tests/orders/order-product.spec.js
+++ client/tests/orders/order-product.spec.js
@@ -24,7 +24,7 @@ test.describe("Create Order From Product", () => {
     await successDialog.getByRole("button", {name: /Continu/i}).click();
     // Delete Order
     await page.goto("/orders");
-    const order = page.getByRole("row").filter({ hasText: "Playwright Customer" });
+    const order = page.getByRole("row").filter({ hasText: "Playwright Order Customer" });
     await order.getByRole("button", { name: "Delete order" }).click();
     const deleteDialog = page.getByRole("dialog", {name: "Delete The Order"});
     await deleteDialog.getByRole("button", {name: "Delete"}).click();
